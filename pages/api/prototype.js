7b068e49030a38b47af7494ac31627d1fa44ef00
tevriq <tevriq@gmail.com>
2026-09-03 18:12:13 +0800

    fix: load P14 state review behavior patch

--- pages/api/prototype.js
+++ pages/api/prototype.js
@@ -4,7 +4,9 @@ import path from 'path';
 export default function handler(req,res){
   const file=path.join(process.cwd(),'index.html');
   const bars='<i></i>'.repeat(24);
-  const html=fs.readFileSync(file,'utf8').replace("${'<i></i>'.repeat(24)}", bars);
+  const html=fs.readFileSync(file,'utf8')
+    .replace("${'<i></i>'.repeat(24)}", bars)
+    .replace('</body>','<script src="/p14-state-patch.js"></script></body>');
   res.setHeader('Content-Type','text/html; charset=utf-8');
   res.setHeader('Cache-Control','no-store');
   res.status(200).send(html);
