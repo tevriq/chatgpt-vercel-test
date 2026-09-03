92b90a2cbe49eddacfe0c8b016132fb599966ebd
tevriq <tevriq@gmail.com>
2026-09-03 17:09:31 +0800

    fix: route Vercel root to P14 prototype

---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default function Home(){
+  return <iframe title="P14 prototype" src="/api/prototype" style={{position:'fixed',inset:0,width:'100vw',height:'100vh',border:0,background:'#f2f2f7'}} />
+}
