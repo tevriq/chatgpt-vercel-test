fb636154a1d6a11e272c20ebd81c64c9246dfa59
tevriq <tevriq@gmail.com>
2026-09-05 16:54:51 +0800

    chore: point preview shell to P1 prototype

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home(){
-  return <iframe title="P14 prototype" src="/api/prototype" style={{position:'fixed',inset:0,width:'100vw',height:'100vh',border:0,background:'#f2f2f7'}} />
+  return <iframe title="P1 0.8.1 prototype" src="/api/prototype" style={{position:'fixed',inset:0,width:'100vw',height:'100vh',border:0,background:'#1b2328'}} />
 }
