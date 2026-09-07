a06ce8cae0a84d1095dfcddfe1382af285699053
tevriq <tevriq@gmail.com>
2026-09-07 16:59:49 +0800

    Fix mobile environment badge breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   darkMode: ['class'],
   content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   theme: {
+    screens: { xs: '360px', sm: '640px', md: '768px', lg: '1024px', xl: '1280px', '2xl': '1536px' },
     extend: {
       colors: {
         border: 'hsl(var(--border))',
